612d8f5a3af13e456f19b6fadb5e0249927f2f06
Michael Ringgaard <ringgaard@google.com>
2017-05-03 16:19:57 +0200

    Add script for building all the code

---
+++ tools/buildall.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+bazel build -c opt \
+  base:* \
+  file:* \
+  frame:* \
+  http:* \
+  myelin:* \
+  nlp/document:* \
+  nlp/kb:* \
+  nlp/parser:* \
+  nlp/web:* \
+  nlp/wiki:* \
+  stream:* \
+  string:* \
+  task:* \
+  tools:* \
+  util:* \
+  web:* \
+  workflow:* \
+
