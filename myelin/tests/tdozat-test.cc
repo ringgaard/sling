e15c98b2b2afe28290b792e6e7468cbb482482b6
Michael Ringgaard <ringgaard@google.com>
2017-06-21 16:29:14 +0200

    Dozat parser test

---
+++ myelin/tests/tdozat-test.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+#include "base/init.h"
+#include "base/logging.h"
+#include "myelin/compute.h"
+#include "myelin/flow.h"
+#include "myelin/graph.h"
+#include "myelin/profile.h"
+#include "myelin/kernel/avx.h"
+#include "myelin/kernel/generic.h"
+#include "myelin/kernel/sse.h"
+#include "myelin/kernel/arithmetic.h"
+
+DEFINE_string(input, "local/tdozat.flow", "input file with flow model");
+
+using namespace sling;
+using namespace sling::myelin;
+
+int main(int argc, char *argv[]) {
+  InitProgram(&argc, &argv);
+
+  // Set up kernel library.
+  Library library;
+  RegisterGenericTransformations(&library);
+  RegisterArithmeticKernels(&library);
+  RegisterAVXKernels(&library);
+  RegisterSSEKernels(&library);
+  RegisterGenericKernels(&library);
+
+  // Load model.
+  Flow flow;
+  flow.set_batch_size(1);
+  CHECK(flow.Load(FLAGS_input));
+
+  // Analyze flow.
+  flow.Analyze(library);
+  DCHECK(flow.IsConsistent());
+
+#if 0
+  std::cout << flow.ToString();
+#endif
+
+  Network network;
+  network.set_profiling(true);
+  CHECK(network.Compile(flow, library));
+
+  // Inspect with: objdump -D -Mintel,x86-64 -b binary -m i386 /tmp/distil.bin
+  Cell *classifier = network.GetCell("classifier");
+
+  classifier->WriteCodeToFile("/tmp/tdozat.bin");
+
+  GraphOptions opts;
+  FlowToDotGraphFile(flow, opts, "/tmp/tdozat.dot");
+
+  // Run instance
+  Instance data(classifier);
+  for (int i = 0; i < 100000; ++i) {
+    data.Compute();
+  }
+
+  Profile profile(&data);
+  std::cout << profile.ASCIIReport() << "\n";
+
+  return 0;
+}
