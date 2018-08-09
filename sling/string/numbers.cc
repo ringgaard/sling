c3ad8e4cae01bade143a08168a7f4d553caa3dfb
Michael Ringgaard <ringgaard@google.com>
2018-08-09 16:50:35 +0200

    Use cfloat header instead of float.h

--- sling/string/numbers.cc
+++ sling/string/numbers.cc
@@ -20,11 +20,11 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
-#include <float.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <cfloat>
 #include <limits>
 #include <memory>
 #include <string>
