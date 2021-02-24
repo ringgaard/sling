d95ac987b5850cd83c67d21d3aa79dc18fb9eeb4
Michael Ringgaard <michael@ringgaard.com>
2021-02-24 20:57:27 +0100

    Build master branch

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -2,6 +2,6 @@
 
 # Update to newest version of code in git repo and rebuild.
 echo "Refresh from git repo" $(date -I -d yesterday)
-git pull origin dev 2>&1
+git pull 2>&1
 tools/buildall.sh 2>&1
 
