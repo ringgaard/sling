57876c8991c6a4d86b9e53d0a84db53f7be38312
Michael Ringgaard <michael@ringgaard.com>
2021-03-23 16:17:56 +0100

    FactGrid wikibase import and builder

---
+++ python/dataset/fgbuild.py
@@ -0,0 +1,121 @@
+# Copyright 2021 Ringgaard Research ApS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Build FactGrid knowledge base and alias tables."""
+
+import sling
+import sling.flags as flags
+import sling.task.workflow as workflow
+import sling.task.data as data
+
+flags.parse()
+
+# Start up workflow system.
+workflow.startup()
+
+# Create workflow.
+wf = workflow.Workflow("factgrid")
+datasets = data.Datasets(wf)
+
+# Resources.
+def res(files, fmt="records/frame"):
+  return wf.resource(files, format=fmt)
+
+fgdir = "data/e/factgrid"
+fg_items = res(fgdir + "/factgrid-items.rec")
+items = res(fgdir + "/items.rec")
+properties = res(fgdir + "/properties.rec")
+xrefs = res(fgdir + "/xrefs.sling", "store/frame")
+xref_config = res("data/wiki/xrefs.sling", "store/frame")
+recon_config = res("data/wiki/recon.sling", "store/frame")
+fg_kb = res(fgdir + "/factgrid-kb.sling", "store/frame")
+
+# Collect xrefs.
+def collect_xrefs():
+  xref = wf.task("xref-builder")
+  wf.connect(wf.read(fg_items), xref)
+  xref.attach_input("config", xref_config)
+  xref.attach_output("output", xrefs)
+
+# Map and reconcile items.
+def reconcile_items():
+  wf.mapreduce(
+    input=fg_items,
+    output=items,
+    mapper="item-reconciler",
+    reducer="item-merger",
+    format="message/frame",
+    params={
+     "indexed": True
+    },
+    auxin={
+     "commons": xrefs,
+     "config": recon_config,
+    })
+
+# Build knowledge base.
+def build_kb():
+  property_catalog = wf.map(properties, "wikidata-property-collector")
+  parts = wf.collect(items, property_catalog, datasets.schema_defs())
+  wf.write(parts, fg_kb, params={"snapshot": True})
+
+# Extract aliases.
+def extract_aliases(language):
+  # Select aliases
+  aliases = wf.map(
+    items,
+    "alias-extractor",
+    params={
+     "language": language,
+    },
+    format="message/alias",
+    name="wikidata-alias-extractor")
+
+  # Group aliases on item id.
+  aliases_by_item = wf.shuffle(aliases)
+
+  # Filter and select aliases.
+  selector = wf.task("alias-selector")
+  selector.add_param("language", language)
+  wf.connect(aliases_by_item, selector)
+  item_aliases = wf.channel(selector, format="message/alias")
+
+  # Group aliases by alias fingerprint.
+  aliases_by_fp = wf.shuffle(item_aliases)
+
+  # Merge all aliases for fingerprint.
+  output = res(fgdir + "/" + language + "/aliases.rec", "records/alias")
+  merger = wf.reduce(aliases_by_fp, output, "alias-merger")
+  return output
+
+# Build name table.
+def build_name_table(aliases, language):
+  builder = wf.task("name-table-builder")
+  wf.connect(wf.read(aliases, name="alias-reader"), builder)
+  repo = res(fgdir + "/" + language + "/name-table.repo", "repository")
+  builder.attach_output("repository", repo)
+
+# Run tasks.
+collect_xrefs()
+reconcile_items()
+build_kb()
+for language in ["en", "de", "fr"]:
+  aliases = extract_aliases(language)
+  build_name_table(aliases, language)
+
+workflow.run(wf)
+
+# Shut down.
+workflow.shutdown()
+
---
+++ python/dataset/fgimport.py
@@ -0,0 +1,121 @@
+# Copyright 2021 Ringgaard Research ApS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Import FactGrid and convert to SLING format."""
+
+import sling
+import gzip
+
+# Map ids into FactGrid namespace.
+def convert_id(idstr):
+  if idstr[0] == "P" or idstr[0] == "Q": return "P8168/" + idstr;
+  return idstr
+
+# Initialize commons store.
+commons = sling.Store()
+n_id = commons["id"]
+n_is = commons["is"]
+n_isa = commons["isa"]
+n_qid = commons["/w/qid"]
+n_pid = commons["P343"]
+n_property = commons["/w/property"]
+n_fg_item_id = commons["P8168"]
+
+wikiconv = sling.WikiConverter(commons, "de")
+
+commons.freeze()
+
+# Read all items from FactGrid dump.
+fin = gzip.open("data/c/wikidata/factgrid.json.gz")
+fout = sling.RecordWriter("data/e/factgrid/factgrid-items.rec")
+fprop = sling.RecordWriter("data/e/factgrid/properties.rec")
+num_items = 0
+for line in fin:
+  # Trim line.
+  if len(line) < 3: continue
+  line = line[:line.rfind(b'}') + 1]
+
+  # Convert from JSON to SLING.
+  store = sling.Store(commons)
+  item, revision = wikiconv.convert_wikidata(store, line)
+
+  # Map ids from FactGrid to Wikidata ids.
+  slots = []
+  fgid = None
+  for name, value in item:
+    if name == n_id:
+      # Convert ids to FG ids.
+      fgid = value
+      value = convert_id(value)
+    elif name == n_qid or name == n_pid:
+      # Add Wikidata redirects for mapped items and properties.
+      name = n_is
+      value = store[value]
+    else:
+      # Map statements.
+      name = store[convert_id(name.id)]
+      if type(value) is sling.Frame:
+        if value.ispublic():
+          value = store[convert_id(value.id)]
+        else:
+          # Map qualifiers.
+          qslots = []
+          for qname, qvalue in value:
+            qname = store[convert_id(qname.id)]
+            if type(qvalue) is sling.Frame:
+              if qvalue.ispublic():
+                qvalue = store[convert_id(qvalue.id)]
+            qslots.append((qname, qvalue))
+          value = store.frame(qslots)
+
+    slots.append((name, value))
+
+  # Add link to FactGrid.
+  if fgid: slots.append((n_fg_item_id, fgid))
+
+  # Write mapped item to output.
+  mapped_item = store.frame(slots)
+  fout.write("P8168/%s" % fgid, mapped_item.data(binary=True))
+  num_items += 1
+
+  # Write property map.
+  if mapped_item[n_isa] == n_property:
+    pid = mapped_item[n_is]
+    if pid is None:
+      pid = mapped_item[n_id]
+    else:
+      pid = pid.id
+    fprop.write("P8168/%s" % fgid, "{=%s}" % pid)
+
+# Add FactGrid xref property.
+store = sling.Store(commons)
+fgprop = store.parse("""
+{
+  =P8168
+  :/w/property
+  name: "FactGrid item ID"@/lang/en
+  description: "identifier for an item in FactGrid"@/lang/en
+  source: /w/entity
+  target: /w/xref
+  P31: P8168/Q21878
+  P1630: "https://database.factgrid.de/wiki/Item:$1"
+}""")
+fout.write("P8168", fgprop.data(binary=True))
+fprop.write("P8168", "{=P8168}")
+
+fin.close()
+fout.close()
+fprop.close()
+print(num_items, "items")
+
